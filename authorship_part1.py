24c40ac1573ca098e67e393e40d2c96010e78070
ThomasHadley <38992736+ThomasHadley@users.noreply.github.com>
2018-11-13 15:04:25 -0800

    Update authorship_part1.py

--- authorship_part1.py
+++ authorship_part1.py
@@ -71,6 +71,22 @@ def compare_signatures(sig1, sig2, weight):
         
     return  score
 
+
+def minmax(category):
+    maxi = 0
+    mini = textDict['emma'][category]
+    for key in textDict:
+        if textDict[key][category] > maxi:
+            maxi = textDict[key][category]
+        if textDict[key][category] < mini:
+            mini = textDict[key][category]
+    diff = maxi - mini
+    print('max is ' + str(maxi) + ' and min is ' + str(mini) + ' and diff is ' + str(diff))
+    for key in textDict:
+        ratio = (textDict[key][category] - mini) / diff
+        rounded = round(ratio, 3)
+        print(textDict[key][0] + ' weighted score is ' + str(rounded))
+
             
 
 if __name__ == '__main__':
@@ -99,6 +115,27 @@ if __name__ == '__main__':
         #features.append(round(phrasePerSent(fid), 3))
        
         print(fid, features)
+        
+        
+        textDict = {'emma': ['austen', 3.755, .038, .015, 24.823, 2.778],
+        'persuasion': ['austen', 3.871, .059, .026, 26.2, 3.179],
+        'sense': ['austen', 3.881, 0.045, 0.017, 28.321, 3.174],
+        'kjv': ['bible', 3.448, 0.013, 0.004, 33.573, 5.131],
+        'poems': ['blake', 3.503, 0.184, 0.097, 19.073, 2.947],
+        'stories': ['bryant', 3.5, 0.071, 0.03, 19.407, 2.411],
+        'busterbrown': ['burgess', 3.517, 0.082, 0.033, 17.991, 1.8],
+        'alice': ['carroll', 3.401, 0.077, 0.033, 20.029, 2.406],
+        'ball': ['chesterton', 3.825, 0.086, 0.042, 20.296, 2.122],
+       'brown': ['chesterton', 3.791, 0.091, 0.044, 22.612, 2.315],
+        'thursday': ['chesterton', 3.774, 0.092, 0.045, 18.496, 2.024],
+        'parents': ['edgeworth', 3.509, 0.04, 0.015, 20.593, 2.754],
+        'moby_dick': ['melville', 3.509, 0.04, 0.015, 20.593, 2.754],
+        'paradise': ['milton', 3.887, 0.093, 0.044, 52.31, 8.165],
+        'caesar': ['shakespeare', 3.444, 0.117, 0.064, 11.943, 2.299],
+        'hamlet': ['shakespeare', 3.464, 0.126, 0.076, 12.028, 2.209],
+        'macbeth': ['shakespeare', 3.465, 0.15, 0.09, 12.134, 2.306],
+        'leaves': ['whitman', 3.696, 0.08, 0.037, 36.443, 5.262]}
+        
         
         emma = ['austen', 3.755, .038, .015, 24.823, 2.778]
         persuasion = ['austen', 3.871, .059, .026, 26.2, 3.179]
