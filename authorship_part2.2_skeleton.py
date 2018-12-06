eb81dfda7a973b5aafe01950979f21fdc65de01f
ThomasHadley <38992736+ThomasHadley@users.noreply.github.com>
2018-12-05 19:42:46 -0800

    Update authorship_part2.2_skeleton.py

--- authorship_part2.2_skeleton.py
+++ authorship_part2.2_skeleton.py
@@ -12,6 +12,7 @@ import nltk
 from nltk.book import *
 from nltk.corpus import gutenberg
 import csv
+import re
 
 
 '''
@@ -108,12 +109,12 @@ Reading and Writing files
 
 def write_signatures(sig_list, o_filename):
     f_out = open(o_filename, 'w', newline = '')
-
+    f_out.write(str(sig_list))
     f_out.close()
    
 def read_signatures(in_filename):
     f_in = open(in_filename)
-
+    
     f_in.close()
     
     
@@ -122,6 +123,16 @@ def read_text(in_filename):
         path = 'corpora/gutenberg/' + in_filename
         file = nltk.data.find(path)
         f_in = open(file)
+        raw = f_in.read()
+        f_in.close()        
+    except:
+        print('failed to open', in_filename)
+        return '' 
+    return raw
+
+def read_mystery_files(in_filename):
+    try:
+        f_in = open(filename)
         raw = f_in.read()
         f_in.close()        
     except:
@@ -141,27 +152,51 @@ def print_sig_table(sig_list):
     for debugging
     '''
     print('list of signatures')
-    headers = ['Author','Avg Word Len','Lexical Div', 'Hapax Ratio', 'Avg Sent Len', 'Avg Sent Comp']
-    print('{:30}{:20}{:20}{:20}{:20}{:20}'.format(headers[0],headers[1],headers[2],headers[3],headers[4],headers[5]))
+    headers = ['Author/Text','Avg Word Len','Lexical Div', 'Hapax Ratio', 'Avg Sent Len', 'Avg Sent Comp']
+    print('{:19}{:15}{:15}{:15}{:15}{:15}'.format(headers[0],headers[1],headers[2],headers[3],headers[4],headers[5]))
     #print('\nAuthor\tAvg Word Len\tLexical Div\tHapax Ratio\tAvg Sent Len\tAvg Sent Comp')
-
+    regA = r'^\w+-\w+'
     for i in range(len(sig_list)):
-        print('\n{:30}{:8}{:20}{:20}{:20}{:20}'.format(sig_list[i][0],round(sig_list[i][1],3),round(sig_list[i][2],3),round(sig_list[i][3],3),round(sig_list[i][4],3),round(sig_list[i][5],3)))
+        text = re.findall(regA, sig_list[i][0])
+        print('\n{:19}{:8}{:15}{:15}{:15}{:15}'.format(str(text[0]),round(sig_list[i][1],3),round(sig_list[i][2],3),round(sig_list[i][3],3),round(sig_list[i][4],3),round(sig_list[i][5],3)))
     
 def print_scores(sig_list, m_sig_list, weights):
     print('\ntable of scores\n')
+    header = ['Name of file', 'Closest Related', 'Score']
+    print('{:30}{:>20}{:>20}'.format(header[0],header[1],header[2],))
     for x in range(len(m_sig_list)):
         lowscore = 1000
         winner = 0
-        print('Results for {}: \n'.format(m_sig_list[x][0]))
+#        print('Results for {}: '.format(m_sig_list[x][0]))
         for i in range(len(sig_list)):
             score = compare_signatures(sig_list[i], m_sig_list[x], weights)
             #print('\t{}: {}\n'.format(sig_list[i][0], round(score,3)))
             if score < lowscore:
                 lowscore = score
                 winner = i
-        print('\t\tMost Likely Related To: {}\n'.format(sig_list[winner][0]))
+        regA = r'^\w+-\w+'
+        text = re.findall(regA, sig_list[winner][0])
+        print('{:30}{:>20}{:>20}\n'.format(m_sig_list[x][0], str(text[0]), round(lowscore,2)))
             #print(str('score = ') + str(score))
+            
+def run():
+    n_files = int(input('enter number of mystery files: '))
+    global m_sig_list
+    m_sig_list = []
+    for f in range(n_files):
+        filename = input('enter name of mystery file: ')
+        raw_text = read_text(filename)
+    
+        '''
+        put mystery files in corpora/gutenberg
+        '''
+        m_words = gutenberg.words(filename)
+        m_sents = gutenberg.sents(filename)
+        m_sig = compute_signature(m_words, m_sents, filename)
+        m_sig_list.append(m_sig)
+    
+    print_scores(sig_list, m_sig_list, weights)
+    print('Enter run() to try again')
 
             
 
@@ -173,17 +208,18 @@ main
 if __name__ == '__main__':
     weights = [11, 33, 50, 0.04, 4]
     sig_list = []
-    testfiles = int(input('enter number of test files: '))
-    fileids = gutenberg.fileids()[:testfiles]
+    fileids = gutenberg.fileids()
     for fid in fileids: 
         # compute features, make a list of features
-        words = gutenberg.words(fid)
-        sents = gutenberg.sents(fid)
-        signature = compute_signature(words, sents, fid)
-        sig_list.append(signature)
+        if fid.startswith('mystery') == False:
+            words = gutenberg.words(fid)
+            sents = gutenberg.sents(fid)
+            signature = compute_signature(words, sents, fid)
+            sig_list.append(signature)
     print_sig_table(sig_list)
     write_signatures(sig_list, 'test.csv')
     
+            
     n_files = int(input('enter number of mystery files: '))
     m_sig_list = []
     for f in range(n_files):
@@ -199,3 +235,4 @@ if __name__ == '__main__':
         m_sig_list.append(m_sig)
     
     print_scores(sig_list, m_sig_list, weights)
+    print('Enter run() to try again')
